17214d6c8701828085212c764a42747af9e74f46
Reuben Clemens <rc3299@drexel.edu>
2024-03-23 01:11:55 -0400

    theme

--- script/main.js
+++ script/main.js
@@ -9,73 +9,73 @@ const signs = [
     {
         first: [3, 21],
         last: [4, 19],
-        sign: 'Aries',
+        sign: 'Abomination',
         text: "There is nothing an Aries cannot achieve once they set their mind to it—no mountain is too high. However, you will also find them nursing a hidden imposter syndrome that can chip away at their confidence if allowed free rein. "
     },
     {
         first: [4, 20],
         last: [5, 20],
-        sign: 'Taurus',
+        sign: 'Antiquarian',
         text: "Loyal to a fault, a Taurean is the most reliable person you can have in your corner when the chips are down. However, they have a stubborn streak a mile wide and can hold a grudge like no one else, so make sure you don't cross them. "
     },
     {
         first: [5, 21],
         last: [6, 20],
-        sign: 'Gemini',
+        sign: 'Arbalest',
         text: "Throw a Gemini to the wolves, and they will come back leading the pack—the air element in this sign means that they can adapt easily to any situation. But their fuse runs short and once they run out of patience with someone, there is no wiggle room for second chances. "
     },
     {
         first: [6, 21],
         last: [7, 22],
-        sign: 'Cancer',
+        sign: 'Bounty Hunter',
         text: "Behind the brooding fortress that a Cancer has erected to protect themselves are abundant reserves of deep, undying love and loyalty. Pity that few will get to experience it because they aren’t the best at communicating what is in their hearts. "
     },
     {
         first: [7, 23],
         last: [8, 22],
-        sign: 'Leo',
+        sign: 'Crusader',
         text: "Born to be under the spotlight, there is nothing that this lion enjoys as much as being the cynosure of all eyes. However, this innate conviction that they are always in the right means that they can often run roughshod over others’ feelings and sentiments. "
     },
     {
         first: [8, 23],
         last: [9, 22],
-        sign: 'Virgo',
+        sign: 'Grave Robber',
         text: "Meticulous, organised and diligent, if the world were to end tomorrow, you would want a Virgo to lead the march into the new dawn. However, that pesky niggle of self-doubt in their head means that they are often harsher on themselves than anybody else can be. "
     },
     {
         first: [9, 23],
         last: [10, 22],
-        sign: 'Libra',
+        sign: 'Hellion',
         text: "If you are looking for someone to lend a comforting shoulder during times of distress and truly put themselves in your shoes, ring up the first Libra in your contacts. This empathetic side of theirs can sometimes get derailed by their inability to make up their mind, compounded by a fear of confrontations, which means that you never truly know which side they stand on."
     },
     {
         first: [10, 23],
         last: [11, 21],
-        sign: 'Scorpio',
+        sign: 'Highwayman',
         text: "The fiery, intense personality of a Scorpio can make any time spent together a wild, dizzying ride. But while they will go the extra mile to take care of your emotional needs, they remain notoriously secretive about their own—good luck cracking open the spine of this closed book. "
     },
     {
         first: [11, 22],
         last: [12, 21],
-        sign: 'Sagittarius',
+        sign: 'Hound Master',
         text: "There is no storyteller quite like a Sagittarius—they can have the entire room hanging on their every word. But while they can show you grand dreams, it can sometimes be hard to pin them down and make them deliver on their promises. "
     },
     {
         first: [12, 22],
         last: [1, 19],
-        sign: 'Capricorn',
+        sign: 'Jester',
         text: "Not everyone can conquer the world but if a Cap were to set out to do it, nothing would deter them until they had accomplished their goal. With a personality that is hardwired in practicality, they can often fail to appreciate nuance and are known to be unforgiving of others’ mistakes. "
     },
     {
         first: [1, 20],
         last: [2, 18],
-        sign: 'Aquarius',
+        sign: 'Leper',
         text: "A deep-thinker with a humanitarian streak, an Aquarian has grand plans to change the world. Shame that they left the party early though because their reclusive nature makes it hard for them to establish bonds with those around them. "
     },
     {
         first: [2, 19],
         last: [3, 20],
-        sign: 'Pisces',
+        sign: 'Man At Arms',
         text: "If you are looking to escape the mundane everyday grind, a Pisces’s imaginative mind can whisk you away into a realm of fantasy. Their kind, nurturing personality can prove to be a double-edged sword though, because their overtly sensitive heart is easily wounded, further compounded by a tendency to play the victim."
     },
 ]
@@ -144,7 +144,7 @@ function activeSign(sign) {
         if (sign == signs[i].sign) {
             title.textContent = signs[i].sign;
             description.textContent = signs[i].text;
-            image.src = `images/${signs[i].sign}.jpg`;
+            image.src = `images/${signs[i].sign}.webp`;
         }
     }
 }
